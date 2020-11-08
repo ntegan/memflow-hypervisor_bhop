66ee90b137203395f27a488c6920ee872108fbb6
ntegan <ntegan1@gmail.com>
2020-11-08 04:58:05 -0600

    begin csgo
    get dwlocalplayer and
    client.dll base address

--- scanflow/src/main.rs
+++ scanflow/src/main.rs
@@ -1,5 +1,6 @@
 use memflow::connector::inventory::ConnectorInventory;
 use memflow::mem::virt_mem::VirtualMemory;
+use memflow::types::Address;
 use memflow::error::*;
 
 use memflow_win32::win32::{Kernel, Win32Process};
@@ -17,6 +18,29 @@ fn main() -> Result<()> {
         .build_default_caches()
         .build()?;
 
+    println!("\n[ hypervisor bhop ]");
+
+
+    let process_name = "csgo.exe";
+    let vk_spacebar = 0x20;
+    let vk_insert = 0x2D;
+    let process_module_name = "client.dll";
+    let dwLocalPlayer = 0xD3DD14;
+    let m_fFlags = 0x104;
+    let dwForceJump = 0x51fbfa8;
+
+    let mut local_player = Address::from(0u32);
+    let process_info = kernel.process_info(process_name)?;
+    let mut process = Win32Process::with_kernel_ref(&mut kernel, process_info);
+    let modules = process.module_list()?;
+    let process_mod = modules.into_iter().find(|m| m.name == process_module_name)
+        .ok_or(Error::Other("Could not find the module"))?;
+    local_player = process.virt_mem.virt_read_addr32(process_mod.base + dwLocalPlayer)?;
+    println!("csgo: got local player {:#?}", local_player);
+    println!("csgo: doing bhop");
+    process.destroy();
+
 
     Ok(())
 }
+
